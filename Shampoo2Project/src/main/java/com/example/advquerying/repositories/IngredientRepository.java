9e76950628cfff253005f5da14847dc399ef360a
DTG-J <grozdanovdt@abv.bg>
2024-03-04 11:03:11 +0200

    updated Anotations

--- Shampoo2Project/src/main/java/com/example/advquerying/repositories/IngredientRepository.java
+++ Shampoo2Project/src/main/java/com/example/advquerying/repositories/IngredientRepository.java
@@ -2,9 +2,11 @@ package com.example.advquerying.repositories;
 
 import com.example.advquerying.entities.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -13,7 +15,8 @@ public interface IngredientRepository extends JpaRepository <Ingredient, Long>{
     List<Ingredient> findByNameStartingWith(String name);
 
     List<Ingredient> findByNameInOrderByPrice(List<String> names);
-
+@Modifying
+@Transactional
     @Query("UPDATE Ingredient AS i SET i.name = concat(i.name, 'Updated')")
     void increasePriceBy10Percent();
 }
