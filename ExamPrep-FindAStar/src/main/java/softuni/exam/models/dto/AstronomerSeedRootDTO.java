a98bc5c1a22407b42f0b90ca8f15262eac378269
DTG-J <grozdanovdt@abv.bg>
2024-03-17 14:58:46 +0200

    Update AstronomerSeedRootDTO.java

--- ExamPrep-FindAStar/src/main/java/softuni/exam/models/dto/AstronomerSeedRootDTO.java
+++ ExamPrep-FindAStar/src/main/java/softuni/exam/models/dto/AstronomerSeedRootDTO.java
@@ -1,4 +1,22 @@
 package softuni.exam.models.dto;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "astronomers")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class AstronomerSeedRootDTO {
+    @XmlElement(name = "astronomer")
+    private List<AstronomerSeedDTO>astronomerSeedDto;
+
+    public List<AstronomerSeedDTO> getAstronomerSeedDto() {
+        return astronomerSeedDto;
+    }
+
+    public void setAstronomerSeedDto(List<AstronomerSeedDTO> astronomerSeedDto) {
+        this.astronomerSeedDto = astronomerSeedDto;
+    }
 }
