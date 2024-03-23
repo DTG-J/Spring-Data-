36fe8267f39f4efdb67043a88cf26f3b3a622edf
DTG-J <grozdanovdt@abv.bg>
2024-03-23 10:29:50 +0200

    Update BaseEntity.java

--- ExamPrep-FindAStar/src/main/java/softuni/exam/models/entity/BaseEntity.java
+++ ExamPrep-FindAStar/src/main/java/softuni/exam/models/entity/BaseEntity.java
@@ -7,15 +7,19 @@ import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
-    public Long getId() {
+    protected BaseEntity() {
+    }
+
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 }
